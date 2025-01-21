1f187fce2727454f7a8aaa62f53fb67a10d885d2
Magudeshgit <magudesh2006@gmail.com>
2025-01-21 15:09:36 +0530

    Configured router: paths

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,23 @@ import './App.css'
 import { Navbar } from './Components/Nav bar/Navbar'
 import { Create_account } from './Screen/Create account/Create_account'
 import { Home } from './Screen/Home/Home'
+import { BrowserRouter, Routes, Route } from 'react-router'
+
 
 function App() {
 
   return (
-   <>
-   <Navbar/>
-   <Home/>
-   {/* <Create_account/> */}
-   </>
+  //  <>
+  //  <Navbar/>
+  //  <Home/>
+  //  {/* <Create_account/> */}
+  //  </>
+  <BrowserRouter>
+    <Routes>
+      <Route index element={<><Navbar/><Home/></>}></Route>
+      <Route path='createaccount/' element={<Create_account/>}/>
+    </Routes>
+  </BrowserRouter>
   )
 }
 
